5444627fa89758c356efad3f49534da6aa8a4c30
FabienYt <101272997+FabienYt@users.noreply.github.com>
2022-06-14 13:21:27 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -39,7 +39,7 @@ fi
 if [ "$DOCKER_TASK" = "lint" ] ; then
   yamllint -sc /ansible/yamllint.yml /ansible/
 
-  ansible-lint -p -c .ansible-lint playbooks/preseed.yml playbooks/run.yml
+  ansible-lint -p -c .ansible-lint playbooks/check_update.yml playbooks/preseed.yml playbooks/run.yml playbooks/update.yml
   ansible-playbook --syntax-check /ansible/playbooks/check_update.yml
   ansible-playbook --syntax-check /ansible/playbooks/preseed.yml
   ansible-playbook --syntax-check /ansible/playbooks/run.yml
